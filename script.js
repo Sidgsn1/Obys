1d0ae750e601f8305377efa451469e359ec6a54d
Sidgsn1 <sidgsn101@gmail.com>
2024-07-10 17:43:01 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl=gsap.timeline();
+function loadingAnimation(){
+    var tl=gsap.timeline();
 
 tl.from(".line h1",{
     y:150,
@@ -31,17 +32,44 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:3.8
+    delay:3.5
 })
 
 tl.from("#page1",{
-    delay:0.2,
+    delay:0.1,
     y:1200,
     opacity:0,
-    duration:0.5,
+    duration:0.9,
     ease:"power4.out"
 })
 
 tl.to("#loader",{
     display:"none"
 })
+
+tl.from("#nav",{
+    opacity:0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:.2
+})
+}
+
+function cursorAnimation(){
+    var crsr=document.querySelector("#crsr");
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4");
+
+}
+
+loadingAnimation();
+cursorAnimation();
